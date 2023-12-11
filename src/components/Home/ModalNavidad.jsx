9cd323736fecab7ae5a7f4d509eb48ba5e4b54ec
Pachi-Dev <pachi.claros@gmail.com>
2023-12-11 09:21:12 -0600

    Add transparent background to modal content and remove close button in ModalNavidad component

--- src/components/Home/ModalNavidad.jsx
+++ src/components/Home/ModalNavidad.jsx
@@ -11,7 +11,6 @@ export function ModalNavidad () {
       centered
       onHide={() => setModalShow(false)}
     >
-      <Modal.Header closeButton onClick={() => setModalShow(false)} />
       <Modal.Body className='m-0 p-0'>
         <video src='/FELICES-FIESTAS-IGECO-2023.mp4' autoPlay loop muted className='w-100' />
       </Modal.Body>
